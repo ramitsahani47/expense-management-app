e9fd0f3a3e89e7cab051910fabc53b5c59d24d81
ramitmc143 <ramit@margadarsicomputers.com>
2026-06-27 17:27:25 +0100

    docker implemented

---
+++ database/init.sql
@@ -0,0 +1,24 @@
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    refresh_token TEXT
+);
+
+
+
+CREATE TABLE IF NOT EXISTS expenses (
+   id SERIAL PRIMARY KEY,
+   title VARCHAR(255) NOT NULL,
+   amount NUMERIC NOT NULL,
+   category VARCHAR(255) NOT NULL,
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   user_id INTEGER NOT NULL,
+   CONSTRAINT fk_user
+           FOREIGN KEY (user_id)
+           REFERENCES users(id)
+           ON DELETE CASCADE
+);
